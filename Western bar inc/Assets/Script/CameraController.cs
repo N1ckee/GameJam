082d095b9bcbfb4a7e89987f4ac93e76cbeaf38d
Schlingking <139239937+Schlingking@users.noreply.github.com>
2023-07-11 19:11:31 +0200

    added zooming
    duuh

--- Western bar inc/Assets/Script/CameraController.cs
+++ Western bar inc/Assets/Script/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -10,11 +11,15 @@ public class CameraController : MonoBehaviour
     public float maxx = 3.8f;
     public float miny = -1f;
     public float maxy = 3.5f;
-
+    public Camera camera;
+    public float viewminimum = 1f;
+    public float viewmaximum = 6f;
+    public float viewsize = 5f;
+    public float zoomspeed = 0.6f;
     public Vector3 newPosition;
     void Start()
     {
-        
+
         newPosition = transform.position;
     }
 
@@ -26,6 +31,18 @@ public class CameraController : MonoBehaviour
 
     void HandleMovementInput()
     {
+        //zoomin in
+        if (Input.GetAxis ("Mouse ScrollWheel")> 0)
+        {
+            viewsize -= zoomspeed;
+            viewsize =Mathf.Clamp(viewsize, viewminimum, viewmaximum);
+        //zoomin out
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            viewsize += zoomspeed;
+            viewsize = Mathf.Clamp(viewsize, viewminimum, viewmaximum);
+        }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition += (transform.up * movementSpeed);
@@ -44,6 +61,9 @@ public class CameraController : MonoBehaviour
         }
         newPosition = new Vector3(Mathf.Clamp(newPosition.x, minx, maxx),Mathf.Clamp(newPosition.y, miny, maxy),newPosition.z);
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+
+        
+        camera.orthographicSize = Mathf.Lerp(camera.orthographicSize,viewsize, Time.deltaTime * movementTime);
     }
 
 }
